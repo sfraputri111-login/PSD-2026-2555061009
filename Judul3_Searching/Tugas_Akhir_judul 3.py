232c2c1d46ea2456b0eca85ac5aa010729cf0436
sfraputri111-login <sfraputri686@gmail.com>
2026-05-06 21:05:23 +0700

    Update Tugas_Akhir_judul 3.py

--- Judul3_Searching/Tugas_Akhir_judul 3.py
+++ Judul3_Searching/Tugas_Akhir_judul 3.py
@@ -1,44 +1,32 @@
-def cari_barang_hilang(daftar_barang, n, target):
-    i = 0
-    hasil = [] 
+def sequential_search(daftar, target):
+    hasil = []
     target = target.lower()
-    
-    while i < n:
-        if target in daftar_barang[i]['nama'].lower():
-            hasil.append(daftar_barang[i])
-        i += 1
+    for barang in daftar:
+        if target in barang['nama'].lower():
+            hasil.append(barang)
     return hasil
 
 def main():
     data_barang = [
-        {"nama": "Kunci Motor Honda", "lokasi": "Di taman"},
-        {"nama": "Kacamata Hitam", "lokasi": "di Jalan Raya"},
-        {"nama": "Tumblr Biru", "lokasi": "Kantin"},
-        {"nama": "Botol Minum", "lokasi": "Ruang kelas 1.2"},
-        {"nama": "Helm", "lokasi": "Parkiran Gedung"},
-        {"nama": "Tas", "lokasi": "Ruang Kelas 2.3"}
+        {"nama": "Tas", "lokasi": "Ruang Tunggu"},
+        {"nama": "Kacamata Hitam", "lokasi": "Jalan Raya"},
+        {"nama": "Botol Minum", "lokasi": "Ruang Kelas"}
     ]
     
-    n = len(data_barang)
-    print("=== SISTEM INFORMASI BARANG Hilang ===")
-    print("-----------------------------------------------------")
+    print("=== SISTEM PENCARIAN BARANG ===")
+    target = input("Cari barang apa? ").strip()
     
-    while True:
-        target = input("Masukkan nama barang yang Anda cari: ").strip()
-        if target: 
-            break
+    if not target:
         print("Input tidak boleh kosong!")
-    hasil_pencarian = cari_barang_hilang(data_barang, n, target)
-
-    print("\n--- Hasil Pencarian ---")
-    if len(hasil_pencarian) > 0:
-        print(f"Ditemukan {len(hasil_pencarian)} data yang cocok:")
-        for idx, barang in enumerate(hasil_pencarian, 1):
-            print(f"{idx}. Barang: {barang['nama']}")
-            print(f"   Lokasi Penyimpanan: {barang['lokasi']}")
-            print("-" * 30)
+        return
+    hasil = sequential_search(data_barang, target)
+    
+    print("\n--- Hasil ---")
+    if hasil:
+        for b in hasil:
+            print(f"- {b['nama']} (Lokasi: {b['lokasi']})")
     else:
-        print(f"Maaf, barang '{target}' tidak ditemukan dalam database temuan.")
+        print("Barang tidak ditemukan.")
 
 if __name__ == "__main__":
     main()
